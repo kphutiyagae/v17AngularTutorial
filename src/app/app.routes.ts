550955a6b53d7b38112e3d7b666228b587181874
kphutiyagae <kopanophutiyagae@gmail.com>
2024-05-02 14:14:48 +0200

    feat(init): :feat: Added Home and Battle pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {BattleComponent} from "./pages/battle/battle.component";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'battle',
+        pathMatch: 'full',
+        component: BattleComponent
+    },
+    {
+        path: '',
+        component: HomeComponent
+    }
+];
