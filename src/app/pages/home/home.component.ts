6666975e8fcc3e0cec7f9d8bac904cbf8842f384
kphutiyagae <kopanophutiyagae@gmail.com>
2024-05-03 13:50:36 +0200

    feat(none): :feat: Added Hero list component.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, signal, WritableSignal} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  title = 'home-component';
+  name: WritableSignal<string> = signal('Dave');
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.name.set('Kopano')
+    }, 2000);
+  }
 }
